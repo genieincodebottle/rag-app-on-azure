27244869924a0e389eb32d8a803324ae074958b6
Rajesh <genieincodebottle@gmail.com>
2025-05-07 22:51:52 +0530

    Fix test case

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,40 +4,6 @@
 # Provisions project-wide infrastructure by composing all modules
 
 # =====================================
-# Terraform Backend Resources
-# =====================================
-
-resource "azurerm_resource_group" "tfstate" {
-  name     = "${var.project_name}-${var.stage}-tfstate-rg"
-  location = var.location
-  
-  tags = {
-    Environment = var.stage
-    Project     = var.project_name
-  }
-}
-
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "${lower(replace(var.project_name, "-", ""))}${var.stage}tfstate"
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                 = azurerm_resource_group.tfstate.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  min_tls_version          = "TLS1_2"
-  
-  tags = {
-    Environment = var.stage
-    Project     = var.project_name
-  }
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
-}
-
-# =====================================
 # Resource Group for all resources
 # =====================================
 
